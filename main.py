7eabbd1e2979032f21ac150afec335be01337b8e
ChuppaWSCW <159850973+ChuppaWSCW@users.noreply.github.com>
2024-05-02 12:27:21 +1200

    MAJOR UPDATE

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # Ask the user their name and save it
- 
+input("What's your name?") 
+
 # Greet the user and introduce the quiz
+print("Welcome user to the quiz")
 
 # Ask the user a question
+input("What is the color of the ocean")
 
 # Tell them the correct answer
+print("It is blue")
 
 # End the quiz
+print("Thank you for playing")
