fc2df3ab5c0f982cc27cb18e3509f40f21d3a2dc
ChuppaWSCW <159850973+ChuppaWSCW@users.noreply.github.com>
2024-05-03 13:00:19 +1200

    v1.2

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Ask the user their name and save it
-input("What's your name?") 
+username = input("What's your name?") 
 
 # Greet the user and introduce the quiz
-print("Welcome user to the quiz")
+print("Welcome user to the quiz", username)
 
 # Ask the user a question
-input("What is the color of the ocean")
-
+answer = input("What is the color of the ocean")
+if answer == "Blue":
+    print("You are correct!")
+else:
+    print("Your Wrong")
 # Tell them the correct answer
 print("It is blue")
 
 # End the quiz
 print("Thank you for playing")
+
