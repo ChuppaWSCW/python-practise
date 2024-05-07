068579f56fa669f806e334d83bec8df18df327df
ChuppaWSCW <159850973+ChuppaWSCW@users.noreply.github.com>
2024-05-07 12:24:52 +1200

    Updatev2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+score=0
+
 # Ask the user their name and save it
 username = input("What's your name?") 
 
@@ -5,14 +7,21 @@ username = input("What's your name?")
 print("Welcome user to the quiz", username)
 
 # Ask the user a question
-answer = input("What is the color of the ocean")
-if answer == "Blue":
-    print("You are correct!")
+answer = input("What is the color of the ocean").upper()
+if answer == "Blue".upper():
+    print("Correct!")
+    score=(score+2)
+elif answer == "":
+    print("You skipped")
 else:
-    print("Your Wrong")
+    print("Wrong!")
+print(" The answer is Blue!")
 # Tell them the correct answer
 print("It is blue")
 
 # End the quiz
 print("Thank you for playing")
 
+
+
+print("Your Score is",score)
