c81be80be70573f2b2ef07d8f8da022b310ff89e
ChuppaWSCW <159850973+ChuppaWSCW@users.noreply.github.com>
2024-05-14 12:26:10 +1200

    updatev4
    4444444444444444444

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
+
+play = "yes"
 score=0
+QUESTION_FORMAT = "{}\nA. {}  B. {} C. {} D. {}" 
 
 # Ask the user their name and save it
 username = input("What's your name?") 
 
 # Greet the user and introduce the quiz
 print("Welcome user to the quiz", username)
-QUESTION_FORMAT = "{}\nA. {}  B. {} C. {} D. {}" 
-# Ask the user a question
-answer = input("What is the color of the ocean").upper()
-if answer == "Blue".upper():
-    print("Correct!")
-    score=(score+2)
-elif answer == "":
-    print("You skipped")
-else:
-    print("Wrong!")
-print(" The answer is Blue!")
-# Tell them the correct answer
-print("It is blue")
 
-# End the quiz
-print("Thank you for playing")
+while play == "yes":
+    # Ask the user a question
+    answer = input("What is the color of the ocean").upper()
+    if answer == "Blue".upper():
+        print("Correct!")
+        score=(score+2)
+    elif answer == "":
+        print("You skipped")
+    else:
+        print("Wrong!")
+    print(" The answer is Blue!")
+    # Tell them the correct answer
+    print("It is blue")
 
+    # End the quiz
+    print("Thank you for playing")
+    print("Well done {}. You finished the quiz. Your final score was {}").format(username, score)
+    play = input("Do you want to play again?").lower()
 
-
-print("Well done {}. You finished the quiz. Your final score was {}").format(username, score)
-
-if answer == "Blue" or answer == "Grey":
-
+print("Goodbye")
