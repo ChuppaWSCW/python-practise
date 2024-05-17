b3068d30d62e7fdddb81c15d5a1bcbb37e35ca23
ChuppaWSCW <159850973+ChuppaWSCW@users.noreply.github.com>
2024-05-17 12:57:53 +1200

    uda
    dadada

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ username = input("What's your name?")
 
 # Greet the user and introduce the quiz
 print("Welcome user to the quiz", username)
+tries = int(input("How many attempts do you want at each question? 1-4"))
+tries = int(tries) 
+
 
 while play == "yes":
-    # Ask the user a question
-    answer = input("What is the color of the ocean").upper()
-    if answer == "Blue".upper():
-        print("Correct!")
-        score=(score+2)
-    elif answer == "":
-        print("You skipped")
-    else:
-        print("Wrong!")
-    print(" The answer is Blue!")
-    # Tell them the correct answer
-    print("It is blue")
+    question_attempts = tries
+    while question_attempts > 0:
+        # Ask the user a question
+        answer = input("What is the color of the ocean").upper()
+        if answer == "Blue".upper():
+            print("Correct!")
+            score=(score+2)
+            break
+        elif answer == "":
+            print("You skipped")
+        else:
+            print("Wrong!")
+        print(" The answer is Blue!")
+        # Tell them the correct answer
+        print("It is blue")
+
 
     # End the quiz
     print("Thank you for playing")
@@ -29,6 +36,3 @@ while play == "yes":
     play = input("Do you want to play again?").lower()
 
 print("Goodbye")
-
-tries = int(input("How many attempts do you want at each question? 1-4"))
-tries = int(tries) 
